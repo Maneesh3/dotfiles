7e90c8569b0038d1e11c6b89a054a360b6f5e195
Maneesh3 <25411456+Maneesh3@users.noreply.github.com>
2024-07-06 22:46:59 -0500

    added nvim init

---
+++ nvim/lua/plugins/custom.lua
@@ -0,0 +1,80 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "typescript", "tsx", "javascript", "python", "go" },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+        },
+        rainbow = {
+          enable = true,
+          extended_mode = true, -- Highlight also non-parentheses delimiters like html tags, boolean or table: lang -> boolean
+          max_file_lines = nil, -- Do not enable for files with more than n lines, int
+        },
+      })
+    end,
+  },
+  -- Add the Rainbow plugin
+  {
+    "p00f/nvim-ts-rainbow",
+  },
+  -- Add Mason for managing external editor tooling
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+    config = function()
+      require("mason").setup({
+        ensure_installed = {
+          "typescript-language-server",
+          "python-lsp-server",
+          "eslint-lsp",
+          "jq",
+          "json-lap",
+          "java-language-server",
+          "gopls",
+        },
+      })
+    end,
+  },
+
+  -- Add Mason LSPconfig to bridge Mason and nvim-lspconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright", "tsserver", "gopls" },
+      })
+    end,
+  },
+
+  -- Add nvim-lspconfig to configure LSP servers
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      -- Configure Pyright for Python
+      lspconfig.pyright.setup({})
+
+      -- Configure tsserver for JavaScript and TypeScript
+      lspconfig.tsserver.setup({})
+
+      -- Configure gopls for Go
+      lspconfig.gopls.setup({})
+    end,
+  },
+}
