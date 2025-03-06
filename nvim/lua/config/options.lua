cf2ed99550dfe559ca3616824f974990bb9f2b7d
Maneesh3 <25411456+Maneesh3@users.noreply.github.com>
2025-03-06 00:23:43 -0600

    added winbar and showtabline

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.winbar = "%=%m %{expand('%:~:.')}"
+--vim.opt.winbar = "%=%m %f"
+vim.opt.showtabline = 2
+-- Winbar text colour
+vim.api.nvim_set_hl(0, "WinBar", { bg = "#1e1e1e", fg = "#ffffff", bold = true }) -- Active window
+vim.api.nvim_set_hl(0, "WinBarNC", { bg = "#3a3a3a", fg = "#aaaaaa", bold = false }) -- Inactive windows
